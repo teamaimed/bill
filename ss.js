b3120211cce1d163cdc0a9ebbf1a0acd22db1c79
TeamAimed <74772606+teamaimed@users.noreply.github.com>
2021-08-15 19:36:01 +0530

    main

---
+++ ss.js
@@ -0,0 +1,14 @@
+  <script type="text/javascript">
+   var _scq = _scq || [];
+   var _scs = _scs || {};
+   _scs.teamId = "2aoekzpsOCpalW063Riw9Z";
+
+   (function() {
+     var dc = document.createElement('script');
+     dc.type = 'text/javascript';
+     dc.async = true;
+     dc.src = '//cdn.sendx.io/prod/2aoekzpsOCpalW063Riw9Z.js';
+     var s = document.getElementsByTagName('script')[0];
+     s.parentNode.insertBefore(dc, s);
+   })();
+  </script>
